618a40bf0a72510ceaff45ddc8ce6e9161bf6ce9
Maxence Bouret <maxence.bouret@gmail.com>
2019-05-02 12:26:43 +0100

    Simple App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Todo List</h1>
+        <form>
+          <input type="text" name="item" placeholder="Your todo item" />
+          <select name="category">
+            <option value="">Select your category</option>
+            <option value="shopping">Shopping</option>
+            <option value="coding">Coding</option>
+            <option value="other">Other</option>
+          </select>
+          <button>Add</button>
+        </form>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Categroy</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>...</td>
+              <td>...</td>
+              <td><button>Delete</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
