6af64eec454e2a192d506d4eb3d4828713c2ec52
Maxence Bouret <maxence.bouret@gmail.com>
2019-05-02 12:44:56 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+// 1st Rule: 1 input/select/textarea = 1 state value
+// 2nd Rule: always initialise the state in the constructor
+// 3rd Rule: always add the attributes `value` and `onChange` for each input/select/textarea
+
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      item: 'Buy carrots', // Inital value for the item
+      category: 'shopping', // Inital value for the category
+    }
+    this.handleChange = this.handleChange.bind(this) // Now, the value of `this` in the method `handleChange` is always going to be the current `this` (the current component)
+  }
+  handleChange(event) {
+    console.log("handleChange triggered")
+    console.log(event)
+    console.log(event.target)
+    console.log(event.target.value)
+    this.setState({
+      item: event.target.value
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Todo List</h1>
         <form>
-          <input type="text" name="item" placeholder="Your todo item" />
-          <select name="category">
+          <input 
+            type="text" 
+            name="item" 
+            value={this.state.item}
+            onChange={this.handleChange}
+            placeholder="Your todo item" />
+          <select 
+            value={this.state.category}
+            onChange={this.handleChange}
+            name="category">
             <option value="">Select your category</option>
             <option value="shopping">Shopping</option>
             <option value="coding">Coding</option>
